b5502b06df07c8bbb60e0986c927bbd9293e2eca
Tsaschikher Ulziibadrakh <tsaschikherulziibadrakh@TesaMacBookPro.local>
2021-09-16 11:46:11 -0400

    PyPoll 1st

---
+++ PyPoll.py
@@ -0,0 +1,46 @@
+# The data we need to retrieve
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each canditae won
+# 5. The winner of the election based on popular vote
+
+##############################################################################################
+##############################################################################################
+#Add our dependencies
+import csv
+import os
+# Assign a variable for the file to load and the path
+file_to_load = os.path.join (".","Resources", "election_results.csv")  
+# Assign a variable to save the file to a path
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+#1. Initialize a total vote counter
+total_votes = 0
+# Candidate Options
+candidate_options = []
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+    #Read and print the header row
+    headers = next(file_reader)
+    #Print each row in the CSV file
+    for row in file_reader:
+        #2. Add to the total vote count
+        total_votes += 1
+        #Print the candidate name from each row
+    candidate_name = row[2]
+        #Add the candidate name to the candidate list
+    candidate_options.append(candidate_name)
+        #Print the candidate list
+        #print(candidate_options)
+        #3. Print the total votes
+    print(total_votes)
+        
+
+
+
+   
+   
+
+
+
